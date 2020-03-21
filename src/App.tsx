65c975ea4370bc44bd8061570741f6cad0b3ff77
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-03-21 22:22:47 +0900

    Areasコンポーネント、Graphコンポーネント作成、SCSSファイル作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import Areas from './Areas'
+import Graph from './Graph'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-header-title">Title</h1>
       </header>
+			<main className="App-main">
+				<Areas />
+				<Graph />
+			</main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
