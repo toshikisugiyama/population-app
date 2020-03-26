4c6fe35230a3b7f196998e67afd9b617f8dd3c2e
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-03-26 15:56:55 +0900

    edit styles

--- src/Prefectures.tsx
+++ src/Prefectures.tsx
@@ -16,7 +16,7 @@ function Prefectures({prefectures, selectPref}: {prefectures: Array<responses>, 
 	}
 	return (
 		<div className="prefecture">
-			<h2 className="prefecture-title">都道府県リスト</h2>
+			<h2 className="prefecture-title">都道府県を選択してください</h2>
 			<ul className="prefecture-list">
 				{prefectures.map((item: responses) => {
 					return (
